672927725c49c498610935303ab61a0310bb18c2
dreas <nisa@local.fr>
2024-01-27 18:38:14 +0700

    hotfix onboard mobile layout
    hotfix onboard mobile layout

--- src/pages/OnBoard.jsx
+++ src/pages/OnBoard.jsx
@@ -1,21 +1,25 @@
 import LogoJakartaUtaraChat from "/logo-jakarta-utara-info-chat.svg"
 import LogoPemkotJakartaUtara from "/logo-pemkot-jakarta-utara.png"
 import { Link } from "react-router-dom"
+import useWindowSize from "../hooks/windowSize"
 
 export default function OnBoard() {
+  const { width } = useWindowSize()
+
   return (
     <section className="h-full grid lg:place-content-center">
       <div className="flex flex-col justify-center lg:flex-row w-full lg:w-[1200px]">
-        <div className="lg:w-[700px] lg:h-[400px] flex items-start pt-16">
+        <div className="flex items-start justify-center lg:justify-start lg:w-[700px] lg:h-[400px] pt-[90px]">
           <img
             src={LogoJakartaUtaraChat}
             alt="LogoJakartaUtaraChat"
-            width={136}
-            className="mt-5 mr-10 ml-16"
+            width={width > 500 ? 136 : 65}
           />
-          <h1 className="font-bold lg:text-[67px] lg:w-[200px] lg:leading-[89px]">
-            Jakarta Utara. Info
-          </h1>
+          <div className="font-bold text-[35px] lg:text-[67px] ml-5 -mt-4 lg:ml-10 lg:-mt-8">
+            <div>Jakarta</div>
+            <div>Utara.</div>
+            <div>Info</div>
+          </div>
         </div>
         <div className="lg:w-[700px] lg:h-[400px] text-center mt-auto lg:border-l pt-24">
           <div className="flex flex-col space-y-3 lg:px-36">
@@ -30,7 +34,7 @@ export default function OnBoard() {
             </Link>
           </div>
 
-          <div className="flex flex-col items-center mt-20">
+          <div className="flex flex-col items-center mt-16">
             <div className="text-sm mb-3">Didukung Oleh</div>
             <img
               src={LogoPemkotJakartaUtara}
